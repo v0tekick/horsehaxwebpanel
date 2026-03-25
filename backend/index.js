a2bd77b529d3e38f8abc7b6a353e2878401d5363
Александр Вербин <aleksandrverbin@MacBook-Air-Aleksandr.local>
2026-03-25 15:33:34 +0400

    Fix: Update Express catch-all route for compatibility with Node.js v23/Express 5

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
 });
 
 app.listen(PORT, () => {
